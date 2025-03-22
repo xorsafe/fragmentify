78cbb61281bd2df49b0dfabc44de0f4830369d56
Nikhil Verma <nikhilverma@xorsafe.com>
2025-03-22 12:54:03 +0530

    Setup of a basic typescript project with jest testing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+}; 
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * A simple function that adds two numbers
+ * @param a First number
+ * @param b Second number
+ * @returns The sum of a and b
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+} 
